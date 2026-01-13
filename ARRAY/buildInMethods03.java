a0a9b9758344e3ea6b3492809f0d5e1c36c6ab19
Akash Maurya <119936164+Akash12219737@users.noreply.github.com>
2026-01-13 14:49:41 +0530

    println changes to print

--- ARRAY/buildInMethods03.java
+++ ARRAY/buildInMethods03.java
@@ -10,7 +10,7 @@ public class buildInMethods03 {
         Arrays.sort(arr);
         System.out.println();
         for(int i=0; i< arr.length;i++){
-            System.out.println(arr[i]+" ");
+            System.out.print(arr[i]+" ");
         }
     }
 }
